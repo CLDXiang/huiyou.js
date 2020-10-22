191da3da8148f2c43225fb5560a4e1def28c3180
Zhang Zhengyi <45653651+zhangzhengyi1024@users.noreply.github.com>
2020-10-22 23:17:45 +0800

    feat: 完善消息传递的类型定义

--- src/types/message.ts
+++ src/types/message.ts
@@ -2,30 +2,40 @@
 
 import { VideoInfo } from './video';
 
-export type MessageType = 'playVideo' | 'pauseVideo';
+export type MessageType = 'playVideo' | 'pauseVideo' | 'fetchVideo' | 'synchronizeTime';
 
+/** 视频开始播放时的消息 */
 interface PlayVideoMessagePayload {
   bvid: string;
-  duration: number;
 }
 
+/** 视频停止播放时的消息 */
 interface PauseVideoMessagePayload {
+  uid: string;
   bvid: string;
+  /** 此视频播放的总时长/秒 */
+  playedTime: number;
+  /** 视频总时长/秒 */
+  totalDuration: number;
 }
 
 export interface MessagePayloadMap {
   playVideo: PlayVideoMessagePayload;
   pauseVideo: PauseVideoMessagePayload;
+  fetchVideo: undefined;
+  synchronizeTime: undefined;
 }
 
+/** 前端脚本向后端脚本通信的信息 */
 export interface Message<K extends MessageType> {
   type: K;
   payload: MessagePayloadMap[K];
 }
 
-type PauseVideoMessageResponse = VideoInfo | null;
-
+/** 后端脚本给前端脚本的响应 */
 export interface MessageResponseMap {
   playVideo: undefined;
-  pauseVideo: PauseVideoMessageResponse;
+  pauseVideo: undefined;
+  fetchVideo: VideoInfo | null;
+  synchronizeTime: number | null;
 }
