b285feb45ea8c6083c799c48dda595e09e82994c
Zhang Zhengyi <45653651+zhangzhengyi1024@users.noreply.github.com>
2020-10-23 00:51:10 +0800

    feat: 完善事件处理函数

--- src/background/messageCallback.ts
+++ src/background/messageCallback.ts
@@ -2,6 +2,8 @@
  * 处理 Content 发来的信息
  */
 import { MessagePayloadMap, MessageResponseMap, MessageType } from '@/types/message';
+import { getRecommendedVideo, recordVideoLocally } from './recordVideo';
+import { getRemainingTime, startTimekeeping } from './timekeeping';
 
 export default function handleMessage(
   message: {
@@ -9,12 +11,34 @@ export default function handleMessage(
     payload: MessagePayloadMap[MessageType];
   },
   sender: chrome.runtime.MessageSender,
-  sendResponse: (response?: MessageResponseMap[MessageType]) => void,
+  sendResponse: <K extends MessageType>(response: MessageResponseMap[K]) => void,
 ) {
   switch (message.type) {
     case 'playVideo':
       {
         const payload = message.payload as MessagePayloadMap['playVideo'];
+        // TODO: 处理 playVideo 事件
+      }
+      break;
+    case 'pauseVideo':
+      {
+        const payload = message.payload as MessagePayloadMap['pauseVideo'];
+        recordVideoLocally(payload);
+      }
+      break;
+    case 'fetchVideo':
+      {
+        const recommendedVideo = getRecommendedVideo();
+        sendResponse<'fetchVideo'>(recommendedVideo);
+        if (recommendedVideo !== null) {
+          startTimekeeping();
+        }
+      }
+      break;
+    case 'synchronizeTime':
+      {
+        const remaining = getRemainingTime();
+        sendResponse<'synchronizeTime'>(remaining);
       }
       break;
     default:
