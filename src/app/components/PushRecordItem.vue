b7524b822e0b1bba05e7bf250d31035413566c8e
carolthepooh <1792618016@qq.com>
2020-10-26 00:14:02 +0800

    feat:push records(添加页面)

---
+++ src/app/components/PushRecordItem.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="wrap">
+    <img
+      class="img"
+      :src="img"
+    >
+    <div class="content">
+      <div
+        class="title"
+        @click="link"
+      >
+        {{ title }}
+      </div>
+      <div style="flex: 1" />
+      <div class="desc">
+        <span>播放量:{{ play }}</span>
+        <span style="margin-left: 100px">{{ tag }}</span>
+        <span style="margin-left:100px">作者:{{ author }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PushRecordItem',
+  props: {
+    img: String,
+    title: String,
+    author: String,
+    pubdate: String,
+    type: String,
+    play: String,
+    tag: String,
+    bv: String,
+  },
+  methods: {
+    link() {
+      window.open(`https://www.bilibili.com/video/${this.bv}`);
+    },
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+  .wrap{
+    margin: 14px 0;
+    display: flex;
+    flex-direction: row;
+    height: 100px;
+    text-align: left;
+    .img{
+      width: 160px;
+      background: red;
+      border-radius: 4px;
+      margin-right: 30px;
+    }
+    .content{
+      flex: 1;
+      border-bottom: 1px #eee solid;
+      padding: 0 0 16px;
+      display: flex;
+      flex-direction: column;
+      .title{
+        font-size: 15px;
+        font-weight: bold;
+        color: black;
+          cursor: pointer;
+      }
+      .desc{
+        font-size: 13px;
+      }
+    }
+  }
+</style>
---
+++ src/app/pages/PushRecords.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="records">
+    <div class="header">
+      <h3>历史记录</h3>
+    </div>
+    <a-timeline>
+      <a-timeline-item
+        v-for="item in records"
+        :key="item.uid"
+      >
+        {{ item.pubdate }}
+        <push-record-item
+          :bv="item.bv"
+          :tag="item.tag"
+          :play="item.play"
+          :type="item.type"
+          :author="item.author"
+          :title="item.title"
+          :img="item.pic"
+        />
+      </a-timeline-item>
+    </a-timeline>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import PushRecordItem from '@/app/components/PushRecordItem.vue';
+import httpClient from '@/apis/index';
+
+export default defineComponent({
+  name: 'PushRecords',
+  components: { PushRecordItem },
+  data() {
+    return {
+      records: {},
+    };
+  },
+  mounted() {
+    httpClient.getRecords().then((data) => {
+      console.log(data);
+      this.records = data;
+    });
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+    .header {
+        display: flex;
+        margin-bottom: 24px;
+    }
+
+    .records {
+        width: 800px;
+        margin: 0 auto;
+    }
+</style>
