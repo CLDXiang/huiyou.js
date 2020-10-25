0b89e830665d4602c05f8cd9fe738a8c806cde8c
Zhang Zhengyi <45653651+zhangzhengyi1024@users.noreply.github.com>
2020-10-25 20:32:31 +0800

    feat: 增加 FetchVideoMessagePayload 类型

--- src/types/message.ts
+++ src/types/message.ts
@@ -13,13 +13,12 @@ export type MessageType =
   | 'synchronizeTime';
 
 /** 视频开始播放时的消息 */
-interface PlayVideoMessagePayload {
+export interface PlayVideoMessagePayload {
   bvid: string;
 }
 
 /** 视频停止播放时的消息 */
 export interface PauseVideoMessagePayload {
-  uid: string;
   bvid: string;
   /** 此视频播放的总时长/秒 */
   playedTime: number;
@@ -27,11 +26,17 @@ export interface PauseVideoMessagePayload {
   totalDuration: number;
 }
 
+/** 要求推送视频时的消息 */
+export interface FetchVideoMessagePayload {
+  /** 用户 id */
+  uid: string;
+}
+
 /** 前端脚本向后端脚本通信的类型与内容的映射 */
 export interface MessagePayloadMap {
   playVideo: PlayVideoMessagePayload;
   pauseVideo: PauseVideoMessagePayload;
-  fetchVideo: undefined;
+  fetchVideo: FetchVideoMessagePayload;
   synchronizeTime: undefined;
 }
 
