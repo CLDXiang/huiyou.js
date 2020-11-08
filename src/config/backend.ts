a67ffc6e3313462b0092f5e81eced5bd341bb95a
Zhang Zhengyi <45653651+zhangzhengyi1024@users.noreply.github.com>
2020-11-09 00:11:56 +0800

    refactor: 把配置项同一放在 config 文件夹里

---
+++ src/config/backend.ts
@@ -0,0 +1,9 @@
+const { freeze } = Object;
+
+/** 后端相关配置 */
+export const BACKEND = freeze({
+  /** 后端接口基础 URL */
+  BASE_URL: 'https://huiyou.fun/api/',
+  RECORD_URL: 'records',
+  VIDEO_URL: 'videos',
+});
