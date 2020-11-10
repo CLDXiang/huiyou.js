ffb09061d0805baa0d87ac5f349379d7f457b7df
Zhang Zhengyi <45653651+zhangzhengyi1024@users.noreply.github.com>
2020-11-10 15:45:54 +0800

    refactor: B 站 API同一放在 bilibiliApi 里

--- src/config/bilibiliApi.ts
+++ src/config/bilibiliApi.ts
@@ -1,7 +1,15 @@
 const { freeze } = Object;
 
-/** B 站 API 接口的 URL */
-export const bilibiliApiUrl = freeze({
+/** B 站数据 API 接口 */
+export const BILIBILI_DATA_API = {
+  /** B 站 API 基础接口 */
+  BASE_URL: 'https://api.bilibili.com/',
+  /** 获取视频基本信息 */
+  VIDEO_INFO_URL: 'x/web-interface/view',
+};
+
+/** 监听用户行为的 B 站 URL */
+export const BILIBILI_LISTENED_URL = freeze({
   /** 点赞请求的 URL */
   like: 'https://api.bilibili.com/x/web-interface/archive/like',
   /** 收藏请求的 URL */
