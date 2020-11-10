8405cf862384cbab4c09ea6b22721f900ea75621
CLDXiang <417198670@qq.com>
2020-11-10 15:07:27 +0800

    fix(app): 接口改为 async/await

--- src/app/apis/bilibili.ts
+++ src/app/apis/bilibili.ts
@@ -9,33 +9,30 @@ const getVideoInfo: (req: {
   bvid: string;
   /** 创建时间 */
   createdAt: Dayjs;
-}) => Promise<RecordDetailItem> = ({ bvid, createdAt }) =>
-  new Promise<RecordDetailItem>((resolve, reject) => {
-    biliRequest.getVideoInfo({ bvid })
-      .then((resp) => {
-        const rawData = resp.data.data;
-        if (rawData) {
-          const parsedData: RecordDetailItem = {
-            ...rawData,
-            createdAt,
-            pubdate: dayjs.unix(rawData.pubdate),
-            ctime: dayjs.unix(rawData.ctime),
-            pic: parseUrl(rawData.pic),
-            owner: {
-              ...rawData.owner,
-              face: parseUrl(rawData.owner.face),
-            },
-            stat: {
-              ...rawData.stat,
-              nowRank: rawData.stat.now_rank,
-              hisRank: rawData.stat.his_rank,
-            },
-          };
-          resolve(parsedData);
-        }
-      })
-      .catch((err) => reject(err));
-  });
+}) => Promise<RecordDetailItem> = async ({ bvid, createdAt }) => {
+  const resp = await biliRequest.getVideoInfo({ bvid });
+  const rawData = resp.data.data;
+  if (rawData) {
+    const parsedData: RecordDetailItem = {
+      ...rawData,
+      createdAt,
+      pubdate: dayjs.unix(rawData.pubdate),
+      ctime: dayjs.unix(rawData.ctime),
+      pic: parseUrl(rawData.pic),
+      owner: {
+        ...rawData.owner,
+        face: parseUrl(rawData.owner.face),
+      },
+      stat: {
+        ...rawData.stat,
+        nowRank: rawData.stat.now_rank,
+        hisRank: rawData.stat.his_rank,
+      },
+    };
+    return parsedData;
+  }
+  throw new Error('No Resp Data');
+};
 
 const biliClient = {
   getVideoInfo,
--- src/app/apis/records.ts
+++ src/app/apis/records.ts
@@ -6,19 +6,14 @@ import { RecordItem } from '../types';
 const getRecords: (req: {
   /** 用户 uid */
   uid: string;
-}) => Promise<RecordItem[]> = ({ uid }) =>
-  new Promise<RecordItem[]>((resolve, reject) => {
-    recordRequest.searchRecords({ uid })
-      .then((resp) => {
-        // TODO: 按照 createdAt 降序排序
-        const parsedData = resp.data.map((item) => ({
-          bvid: item.bvid,
-          createdAt: dayjs.unix(item.time),
-        }));
-        resolve(parsedData);
-      })
-      .catch((err) => reject(err));
-  });
+}) => Promise<RecordItem[]> = async ({ uid }) => {
+  const resp = await recordRequest.searchRecords({ uid });
+  const parsedData = resp.data.map((item) => ({
+    bvid: item.bvid,
+    createdAt: dayjs.unix(item.time),
+  }));
+  return parsedData;
+};
 
 const recordClient = {
   getRecords,
