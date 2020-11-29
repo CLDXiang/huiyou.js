b7efc6eee667297efe2b13dfdb132114e4ada805
Zhang Zhengyi <45653651+zhangzhengyi1024@users.noreply.github.com>
2020-11-29 13:07:24 +0800

    chore: api 异常处理

--- src/popup/apis/bilibili.ts
+++ src/popup/apis/bilibili.ts
@@ -3,21 +3,25 @@ import { PlayVideoInfo, VideoShot } from '@/types/video';
 import { biliRequest } from '@/apis';
 
 export async function GetVideoShot(aid: number): Promise<VideoShot | null> {
-  const resp = await biliRequest.getVideoShot({ aid });
-  const rawData = resp.data.data;
-  if (rawData) {
-    const parsedData: VideoShot = {
-      pvdata: rawData.pvdata,
-      imgXLen: rawData.img_x_len,
-      imgYLen: rawData.img_y_len,
-      imgXSize: rawData.img_x_size,
-      imgYSize: rawData.img_y_size,
-      image: rawData.image,
-      index: rawData.index,
-    };
-    return parsedData;
+  try {
+    const resp = await biliRequest.getVideoShot({ aid });
+    const rawData = resp.data.data;
+    if (rawData) {
+      const parsedData: VideoShot = {
+        pvdata: rawData.pvdata,
+        imgXLen: rawData.img_x_len,
+        imgYLen: rawData.img_y_len,
+        imgXSize: rawData.img_x_size,
+        imgYSize: rawData.img_y_size,
+        image: rawData.image,
+        index: rawData.index,
+      };
+      return parsedData;
+    }
+    return null;
+  } catch (error) {
+    return null;
   }
-  throw new Error('No Resp Data');
 }
 
 export async function getVideoInfo(bvid: string): Promise<PlayVideoInfo | null> {
