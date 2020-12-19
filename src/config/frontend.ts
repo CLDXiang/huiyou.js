ff842c232d94cab5383bc33f39e9f201c6ca4243
Zhang Zhengyi <45653651+zhangzhengyi1024@users.noreply.github.com>
2020-12-19 20:39:31 +0800

    feat: 导出配置项

--- src/config/frontend.ts
+++ src/config/frontend.ts
@@ -1,21 +1,10 @@
 import { DEBUG_MODE } from '@/utils/config';
+import { makeOption } from '@/utils/options';
 
 const { freeze } = Object;
 
-/** 计时模块常量 */
-export const TIMEKEEPING = freeze({
-  /** 计时时长/ms */
-  DURATION: DEBUG_MODE ? 70000 : 7000,
-});
-
 /** 视频记录模块常量 */
 export const RECORD_VIDEO = freeze({
-  /** 视频时长上限/秒 */
-  DURATION_UPPER_LIMIT: 60 * 15,
-  /** 视频播放比例下限 */
-  PLAYED_TIME_PROPORTION_LOWER_LIMIT: DEBUG_MODE ? 0 : 1 / 3,
-  /** 视频记录个数下限 */
-  VIDEO_COUNT_LOWER_LIMIT: DEBUG_MODE ? 1 : 5,
   /** 缓存的预拉取视频数 */
   VIDEO_CACHE_COUNT: 5,
 });
@@ -24,10 +13,6 @@ export const RECORD_VIDEO = freeze({
 export const FETCH_VIDEO = freeze({
   /** 视频所在分区 */
   KEYWORD: 'VLOG',
-  /** 视频播放量上限 */
-  AMOUNT_OF_PLAY_UPPER_LIMIT: 0,
-  /** 视频长度下限/秒 */
-  VIDEO_DURATION_LOWER_LIMIT: 60,
   /** 拉取视频的起始页数 */
   START_PAGE: 400,
   /** 拉取视频的结束页数 */
@@ -35,3 +20,18 @@ export const FETCH_VIDEO = freeze({
   /** 拉取视频的时间间隔/毫秒 */
   FETCH_VIDEO_TIMEOUT: 60 * 1000,
 });
+
+export const USER_OPTIONS = makeOption(freeze({
+  /** 弹框显示时长/ms */
+  POPUP_DURATION: DEBUG_MODE ? 70000 : 7000,
+  /** 推荐的视频的播放量上限 */
+  AMOUNT_OF_PLAY_UPPER_LIMIT: 0,
+  /** 推荐的视频的长度下限/秒 */
+  VIDEO_DURATION_LOWER_LIMIT: 60,
+  /** 至少记录几个视频后可以推送视频 */
+  VIDEO_COUNT_LOWER_LIMIT: DEBUG_MODE ? 1 : 5,
+  /** 记录的视频的播放比例下限 */
+  PLAYED_TIME_PROPORTION_LOWER_LIMIT: DEBUG_MODE ? 0 : 1 / 3,
+  /** 记录的视频的时长上限/秒 */
+  DURATION_UPPER_LIMIT: 60 * 15,
+}));
