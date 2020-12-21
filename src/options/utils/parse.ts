bfe7c89a0bf64a1f6397848622a1d6f75b231884
Zhang Zhengyi <45653651+zhangzhengyi1024@users.noreply.github.com>
2020-12-21 11:27:56 +0800

    refactor: 修改变量名

--- src/options/utils/parse.ts
+++ src/options/utils/parse.ts
@@ -1,14 +1,14 @@
 /** string 转 number，遇到 NaN 用缺省值 */
-export function parseStringToNumber(mayBeAString: unknown, or: number) {
+export function parseStringToNumber(mayBeAString: unknown, defaultValue: number) {
   if (typeof mayBeAString === 'string') {
     const tryIt = parseFloat(mayBeAString);
     if (Number.isNaN(tryIt)) {
-      return or;
+      return defaultValue;
     }
     return tryIt;
   }
   if (typeof mayBeAString === 'number' && !Number.isNaN(mayBeAString)) {
     return mayBeAString;
   }
-  return or;
+  return defaultValue;
 }
